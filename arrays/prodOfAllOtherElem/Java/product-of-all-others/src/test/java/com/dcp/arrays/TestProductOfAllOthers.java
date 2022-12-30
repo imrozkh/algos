3ecc3f521d2187323d84e1b0d085fdbaa700cbc3
Imroz Khan <kh.imroz@gmail.com>
2022-12-30 19:47:08 +0530

    Java implementation  for division method.

---
+++ arrays/prodOfAllOtherElem/Java/product-of-all-others/src/test/java/com/dcp/arrays/TestProductOfAllOthers.java
@@ -0,0 +1,15 @@
+package com.dcp.arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class TestProductOfAllOthers {
+    @Test
+    public void testDevisionMethod() {
+        int[] testArr = { 1, 2, 3, 4, 5 };
+        int[] expectedOut = { 120, 60, 40, 30, 24 };
+        int[] outArr = PorodOfNums.byDivision(testArr);
+        assertArrayEquals(expectedOut, outArr);
+    }
+
+}
