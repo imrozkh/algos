00641c116bec6623b4c3d528d71e4f8b4ad65fc7
Imroz Khan <kh.imroz@gmail.com>
2022-12-31 00:31:17 +0530

    code for nodiv method to implement hte prod of all others

--- arrays/prodOfAllOtherElem/Java/product-of-all-others/src/main/java/com/dcp/arrays/PorodOfNums.java
+++ arrays/prodOfAllOtherElem/Java/product-of-all-others/src/main/java/com/dcp/arrays/PorodOfNums.java
@@ -20,7 +20,47 @@ public class PorodOfNums {
     }
 
     public static int[] noDivision(int[] testArr) {
-        return null;
+        int[] outarr = new int[testArr.length];
+        int[] prefArr = getPrefixProductArray(testArr);
+        int[] sufArr = getSuffixProductArray(testArr);
+        for(int i=0; i< testArr.length ; i++) {
+            if(i==0){
+                outarr[i] = sufArr[i+1];
+            }else if(i == testArr.length -1){
+                outarr[i] = prefArr[i-1];
+            }
+            else{
+                outarr[i] = prefArr[i-1] * sufArr[i+1];
+            }
+        }
+
+        return outarr;
+    }
+
+    public static int[] getPrefixProductArray (int [] inArr){
+        int[] prefProdArrr = new int[inArr.length];
+        for( int i=0; i< inArr.length;i++){
+            if(i==0) { // first element should go as is 
+                prefProdArrr[i] = inArr[i];
+            }
+            else{
+                prefProdArrr[i] = prefProdArrr[i-1] * inArr[i];
+            }
+        }
+
+        return prefProdArrr;
+    }
+
+    public static int[] getSuffixProductArray(int[] testArr) {
+        int[] suffixProdArr = new int[testArr.length];
+        for(int i=  testArr.length - 1; i>=0 ; i-- ){
+            if( i == testArr.length-1){
+                suffixProdArr[i] = testArr[i];
+            }else{
+                suffixProdArr[i] = suffixProdArr[i+1] * testArr[i];
+            }
+        }
+        return suffixProdArr;
     }
 
 }
--- arrays/prodOfAllOtherElem/Java/product-of-all-others/src/test/java/com/dcp/arrays/TestProductOfAllOthers.java
+++ arrays/prodOfAllOtherElem/Java/product-of-all-others/src/test/java/com/dcp/arrays/TestProductOfAllOthers.java
@@ -1,6 +1,10 @@
 package com.dcp.arrays;
 
 import static org.junit.Assert.assertArrayEquals;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class TestProductOfAllOthers {
@@ -19,6 +23,23 @@ public class TestProductOfAllOthers {
         int[] outArr = PorodOfNums.noDivision(testArr);
         assertArrayEquals(expectedOut, outArr);
     }
-    
+
+    @Test
+    public void testPrefixProdArray() {
+        int [] outarr = null;
+        int[] testArr     = { 1, 2, 3, 4};
+        int[] expectedArr = { 1, 2, 6, 24};
+        outarr = PorodOfNums.getPrefixProductArray(testArr);
+        assertArrayEquals(expectedArr, outarr);
+    }
+
+    @Test
+    public void testSufixProdArray() {
+        int [] outarr = null;
+        int[] testArr     = { 1, 2, 3, 4};
+        int[] expectedArr = { 24, 24, 12, 4};
+        outarr = PorodOfNums.getSuffixProductArray(testArr);
+        assertArrayEquals(expectedArr, outarr);
+    }
 
 }
